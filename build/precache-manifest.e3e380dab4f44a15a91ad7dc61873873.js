05c02f71e7dfe4e67add5b0cc65c5878ff8e6a25
zain abo almagd <zai1999n@gmail.com>
2025-01-14 07:50:57 +0400

    solved deploying problem on github finally!!!

---
+++ build/precache-manifest.e3e380dab4f44a15a91ad7dc61873873.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47412a5386522f8324674859717ea3fb",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "b71330855d325655590c",
+    "url": "/robo-friends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "7818b2cb79056ecc9f48",
+    "url": "/robo-friends/static/css/main.042ab44e.chunk.css"
+  },
+  {
+    "revision": "b71330855d325655590c",
+    "url": "/robo-friends/static/js/2.cd64d29d.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/robo-friends/static/js/2.cd64d29d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0435292880e02ff02935",
+    "url": "/robo-friends/static/js/3.ecd3194e.chunk.js"
+  },
+  {
+    "revision": "7818b2cb79056ecc9f48",
+    "url": "/robo-friends/static/js/main.1e6f4d03.chunk.js"
+  },
+  {
+    "revision": "6e66549337c7fa3c17ea",
+    "url": "/robo-friends/static/js/runtime-main.92b95875.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
